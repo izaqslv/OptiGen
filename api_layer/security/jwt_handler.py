83e3291dd13ec2098d952ef290f19e951a457417
Izaq S <izaqslv@gmail.com>
2026-04-04 17:57:49 -0300

    fix: swagger oauth token url

--- api_layer/security/jwt_handler.py
+++ api_layer/security/jwt_handler.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-swagger")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login-swagger")
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
