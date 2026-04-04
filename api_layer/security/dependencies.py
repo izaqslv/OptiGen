86973e4d736fd7d38f43bc1270bb5ceed9cd290b
Izaq S <izaqslv@gmail.com>
2026-04-04 19:49:47 -0300

    update: auth + swagger fix + reset db control

--- api_layer/security/dependencies.py
+++ api_layer/security/dependencies.py
@@ -9,7 +9,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login-swagger")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
